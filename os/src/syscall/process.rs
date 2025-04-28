7755ece2edea1e5d557ab7e0882ee7f73816b55a
bugfly66 <bugfly66@qq.com>
2025-04-28 19:56:49 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,7 @@
 //! Process management syscalls
-use alloc::sync::Arc;
-
+use crate::mm::VirtAddr;
+use crate::mm::VirtPageNum;
+use crate::timer::get_time_us;
 use crate::{
     loader::get_app_data_by_name,
     mm::{translated_refmut, translated_str},
@@ -9,6 +10,7 @@ use crate::{
         suspend_current_and_run_next,
     },
 };
+use alloc::sync::Arc;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -67,7 +69,11 @@ pub fn sys_exec(path: *const u8) -> isize {
 /// If there is not a child process whose pid is same as given, return -1.
 /// Else if there is a child process but it is still running, return -2.
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
-    trace!("kernel::pid[{}] sys_waitpid [{}]", current_task().unwrap().pid.0, pid);
+    trace!(
+        "kernel::pid[{}] sys_waitpid [{}]",
+        current_task().unwrap().pid.0,
+        pid
+    );
     let task = current_task().unwrap();
     // find a child process
 
@@ -105,12 +111,40 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    trace!("kernel: sys_get_time");
+    let us = get_time_us();
+    let pte = current_task()
+        .unwrap()
+        .inner_exclusive_access()
+        .memory_set
+        .translate(VirtPageNum::from(VirtAddr::from(
+            (ts as usize) & (!0 << 12 as usize) as usize,
+        )));
+    match pte {
+        Some(x) => {
+            let mut pa = x.ppn().0 as *mut TimeVal;
+            pa = ((pa as usize) << 12).wrapping_add((ts as usize) & 0xfff) as *mut TimeVal;
+            unsafe {
+                *pa = TimeVal {
+                    sec: us / 1_000_000,
+                    usec: us % 1_000_000,
+                };
+            }
+        }
+        None => {
+            // sys_mmap(ts as usize, 4096, 3);
+            // let _pte2 = current_memory_set()
+            //     .exclusive_access()
+            //     .translate(VirtPageNum::from(ts as usize));
+            panic!("TimeVal's address is not readable or writable");
+        }
+    }
+    0
 }
 
 /// YOUR JOB: Implement mmap.
@@ -149,12 +183,12 @@ pub fn sys_spawn(_path: *const u8) -> isize {
         current_task().unwrap().pid.0
     );
     let res = sys_fork();
-   if res == -1 {
-       return -1;
-   } else {
-       sys_exec(_path);
-   } 
-   res
+    if res == -1 {
+        return -1;
+    } else {
+        sys_exec(_path);
+    }
+    res
 }
 
 // YOUR JOB: Set task priority.
